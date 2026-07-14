144bb92cfc3cc65fd039ebb4968dbd5a4d86aa5e
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-07-14 22:29:33 +0800

    feat(ui): implemented a `framer-motion` based animation on the contacts button

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,12 +1,41 @@
+'use client'
+
 import Image from "next/image"
-import ContactButton from "../ui/ContactButton";
+import { motion, Variants } from "framer-motion"
+
+const Line = ({ className }: { className: string }) => {
+  return <div className={`absolute border-0 bg-white ${className}`} />;
+};
+
+const divToRight = {
+  rest: {
+    color: "black",
+    width: 0,
+    transition: {
+      duration: 0.2,
+      type: "tween",
+      ease: "easeIn",
+    },
+  },
+  hover: {
+    color: "black",
+    width: '200px',
+    transition: {
+      duration: 0.5,
+      type: "tween",
+      ease: "easeOut",
+    },
+  },
+} as const satisfies Variants;
 
 export default function TopBar() {
+
     const items = [
         {name: "Home", link: "/"},
         {name: "Works", link: "/"},
         {name: "About Me", link: "/"},
     ]
+
     return (
       <div className="flex bg-(--color-brand-purple) w-full h-fit justify-between items-center px-5 py-5 mt-5 ">
         <div className="flex gap-2.5 w-[50%] items-center justify-start">
@@ -30,7 +59,22 @@ export default function TopBar() {
             ))}
           </div>
 
-          <ContactButton text={"Contacts"}/>
+          <motion.div
+            className="flex h-full w-fit relative items-center overflow-hidden justify-center text-[0.9rem] font-bold bg-[repeating-linear-gradient(315deg,var(--color-line)_0,var(--color-line)_1px,transparent_1px,transparent_50%)] bg-size-[8px_8px] px-5 py-3 hover:cursor-pointer border border-dashed border-white"
+            initial="rest"
+            whileHover="hover"
+            animate="rest"
+          >
+            <motion.div
+              className="rotate-x-30 bg-black z-0 h-20 w-10 absolute -left-10"
+              variants={ divToRight }
+            />
+           
+            <a href="" className="text-white font-light z-50">
+              <p>Contacts</p>
+            </a>
+            
+          </motion.div>
         </div>
       </div>
     );
